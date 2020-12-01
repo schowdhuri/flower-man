dc5f12e8ed9a852efd0eef86f078f018bffcb779
Subir <schowdhuri@users.noreply.github.com>
2020-12-02 00:51:04 +0530

    Updates

---
+++ precache-manifest.fc6ff8c1cd2c726e1aad05d7e73367dc.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d718abf5b52e111f7dd005b58f6b2282",
+    "url": "/flower-man/index.html"
+  },
+  {
+    "revision": "8bb3d17e4d47608c2a0f",
+    "url": "/flower-man/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "2d9399dbacd9647a2ce5",
+    "url": "/flower-man/static/css/main.40391539.chunk.css"
+  },
+  {
+    "revision": "8bb3d17e4d47608c2a0f",
+    "url": "/flower-man/static/js/2.1220c847.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/flower-man/static/js/2.1220c847.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d9399dbacd9647a2ce5",
+    "url": "/flower-man/static/js/main.13dc992b.chunk.js"
+  },
+  {
+    "revision": "cff2cca54f69cc6a4335",
+    "url": "/flower-man/static/js/runtime-main.a05a7c84.js"
+  },
+  {
+    "revision": "b8489f7c1d5c41ac6f58f14e4b0e6898",
+    "url": "/flower-man/static/media/Logo.b8489f7c.png"
+  },
+  {
+    "revision": "0738679df4cf4e566f60343830da7bfa",
+    "url": "/flower-man/static/media/Quicksand-Bold.0738679d.ttf"
+  },
+  {
+    "revision": "865b25e449878b6b4d1f9e098ff2f510",
+    "url": "/flower-man/static/media/Quicksand-Medium.865b25e4.ttf"
+  },
+  {
+    "revision": "216d43ee8707910af457af569eda1dec",
+    "url": "/flower-man/static/media/Quicksand-Regular.216d43ee.ttf"
+  },
+  {
+    "revision": "c82b184bf7450e14adccb7b0d6117474",
+    "url": "/flower-man/static/media/Quicksand-SemiBold.c82b184b.ttf"
+  },
+  {
+    "revision": "dd7e295e9faa99f4a691448d0f4a8a29",
+    "url": "/flower-man/static/media/bridal-stage.dd7e295e.jpg"
+  },
+  {
+    "revision": "9236e8e34a054ec769ad94345c2d9526",
+    "url": "/flower-man/static/media/contact_flowers.9236e8e3.jpg"
+  },
+  {
+    "revision": "f206c80bbe04441d99a00bf4376c9ae3",
+    "url": "/flower-man/static/media/design1.f206c80b.jpg"
+  },
+  {
+    "revision": "1c208a49bd1acac315b214da9385a597",
+    "url": "/flower-man/static/media/design2.1c208a49.jpg"
+  },
+  {
+    "revision": "fdac3559f5ff62c245eecd3c209df1a2",
+    "url": "/flower-man/static/media/design3.fdac3559.jpg"
+  },
+  {
+    "revision": "9fdc7407870e33a271a432918904fd2c",
+    "url": "/flower-man/static/media/designs.9fdc7407.jpg"
+  },
+  {
+    "revision": "2717f3a0e7c16a4f1a2e5deb3813f673",
+    "url": "/flower-man/static/media/engagements.2717f3a0.jpg"
+  },
+  {
+    "revision": "47293fa144692fff951e1109510bb905",
+    "url": "/flower-man/static/media/event1.47293fa1.jpg"
+  },
+  {
+    "revision": "78cbcf7ca0f2d549e19bddb3bae6ec75",
+    "url": "/flower-man/static/media/event2.78cbcf7c.jpg"
+  },
+  {
+    "revision": "0b4e88dc70a9b6911db561e40101a384",
+    "url": "/flower-man/static/media/event3.0b4e88dc.jpg"
+  },
+  {
+    "revision": "906bd7938641b15650df7bb12c0497e6",
+    "url": "/flower-man/static/media/flower_carousel_1.906bd793.jpg"
+  },
+  {
+    "revision": "08a5ec846b1016ac05c3c1f0bf054af0",
+    "url": "/flower-man/static/media/flower_carousel_2.08a5ec84.jpg"
+  },
+  {
+    "revision": "b96f799ffc21d410c1a955b92188d435",
+    "url": "/flower-man/static/media/flower_carousel_3.b96f799f.jpg"
+  },
+  {
+    "revision": "30c3847fa94b6a0cc7e8bbbc87a4b235",
+    "url": "/flower-man/static/media/flowers1.30c3847f.jpg"
+  },
+  {
+    "revision": "6ef8f6c87f3138528189a1b6648ec136",
+    "url": "/flower-man/static/media/flowers2.6ef8f6c8.jpg"
+  },
+  {
+    "revision": "c8a77e4d301a90a50de7b4df9a085fff",
+    "url": "/flower-man/static/media/instagram.c8a77e4d.png"
+  },
+  {
+    "revision": "1e52dd75b3151a2ae6b451a02ac16a10",
+    "url": "/flower-man/static/media/intro.1e52dd75.mov"
+  },
+  {
+    "revision": "2430c14438f0b8b93b43959d71ac48a4",
+    "url": "/flower-man/static/media/pinterest.2430c144.png"
+  },
+  {
+    "revision": "236de94b2db38ce8550dcd9b44c65973",
+    "url": "/flower-man/static/media/profile.236de94b.jpg"
+  },
+  {
+    "revision": "bac1cc313f8071dfef62903fcc0b4e6b",
+    "url": "/flower-man/static/media/services.bac1cc31.jpg"
+  },
+  {
+    "revision": "1d590631ca96db243670d86353a96743",
+    "url": "/flower-man/static/media/special.1d590631.jpg"
+  },
+  {
+    "revision": "41f2029b06d4eb8f0ac875311401705a",
+    "url": "/flower-man/static/media/tabletop.41f2029b.jpg"
+  }
+]);
